099bde45a565fee1fe6b03b4c25a7b37d3febf30
arahim306 <arahim306@ufl.edu>
2018-09-10 22:02:15 -0400

    Add files via upload

--- listingController.js
+++ listingController.js
@@ -3,48 +3,33 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
-	$scope.addListing = function(){		
-		$scope.listings.push({ 'code':$scope.code, 'name': $scope.name, 'latitude':$scope.latitude, 'longitude': $scope.longitude, 'address':$scope.address });
-		$scope.code='';
-		$scope.name='';
-		$scope.latitude='';
-		$scope.longitude='';
-		$scope.address='';
-	};
-
-    $scope.deleteListing = function(index) {
-    	$scope.listings.splice(index, 1);
-	};
-
-    $scope.showDetails = function(index) {
-    	try {
-    		$scope.latitude = $scope.listings[index].coordinates.latitude;
-    	}
-    	catch(err) {
-    		$scope.latitude = '';
-    	}
-
-    	try {
-    		$scope.longitude = $scope.listings[index].coordinates.longitude;
-    	}
-    	catch(err) {
-    		$scope.longitude = '';
-    	}
-
-    	try {
-    		$scope.address = $scope.listings[index].address;
-    	}
-    	catch(err) {
-    		$scope.address = '';
-    	}
+    /* 
+      Implement these functions in the controller to make your application function 
+      as described in the assignment spec. 
+     */
+    $scope.addListing = function() {
+      $scope.listings.push({
+            'code': document.getElementById("newEntry").elements["newCode"].value, 
+            'name': document.getElementById("newEntry").elements["newName"].value, 
+            'coordinates': {
+                'latitude': document.getElementById("newEntry").elements["newLatitude"].value, 
+                'longitude': document.getElementById("newEntry").elements["newLongitude"].value
+            },
+            'address': document.getElementById("newEntry").elements["newAddress"].value } );
     };
 
-	$scope.clearText = function(){		
-		$scope.code='';
-		$scope.name='';
-		$scope.latitude='';
-		$scope.longitude='';
-		$scope.address='';
-	};  
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index,1);
+    };
+    $scope.showDetails = function(index) {
+      $scope.code = $scope.listings[index].code;
+      $scope.name = $scope.listings[index].name;
+      try{$scope.latitude = $scope.listings[index].coordinates.latitude;}
+      catch(err){$scope.latitude = '';}
+      try{$scope.longitude = $scope.listings[index].coordinates.longitude;}
+      catch(err){$scope.longitude = '';}
+      try{$scope.address = $scope.listings[index].address;}
+      catch(err){$scope.address = '';}
+    };
   }
 ]);
